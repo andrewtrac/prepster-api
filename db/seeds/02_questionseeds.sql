65cc42cb664bbe7b24e06f58262107fcc61fb327
Andrew Trac <andrew@gigeco.net>
2021-01-13 08:44:19 -0500

    added more seeds to file

--- db/seeds/02_questionseeds.sql
+++ db/seeds/02_questionseeds.sql
@@ -1,4 +1,8 @@
 INSERT INTO questions 
 (question, categories)
 VALUES 
-('You have 100 new emails after coming back from vacation. What do you do?', '{time management}'); 
+('You have 100 new emails after coming back from vacation. What do you do?', '{time management}'), 
+('Can you tell me about a time when you demonstrated leadership skills?', '{leadership}'), 
+('What was a challenging project, and how did you manage it?', '{project management}'), 
+('What would you do if you there was a breakdown in communication at work?', '{communication}'), 
+('How do you feel about working on a team? ', '{teamwork}');
